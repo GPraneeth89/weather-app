875bac5a364e3a719d0546dcada023bf6455ffd3
GPraneeth89 <gpraneeth89@gmail.com>
2021-01-01 23:14:35 +0530

    new commit

--- weathrm/settings.py
+++ weathrm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yzm%d36)p%tnq#^c3=x)nn8w0*0pst&i1m%zhix@#ybxc4!^=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = [
+    'static',
+]
+django_heroku.settings(locals())
